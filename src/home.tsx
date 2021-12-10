0b4bc131a258b1ccecdc935e4583906b6c75f581
cristian <cristian_3dg@hotmail.com>
2021-12-09 20:29:47 -0500

    initial commit

---
+++ src/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+export default function Home(){
+    return (
+    <View>
+        <Text>Home</Text>
+    </View>
+    );
+}
